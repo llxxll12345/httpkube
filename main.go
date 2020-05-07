8b756caff447d9c4bdbbc9a1a928d059b2874507
liulixing <liulixing@google.com>
2020-05-07 16:22:12 -0400

    init

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+	host, _ := os.Hostname()
+	fmt.Fprintf(w, "Hello, world!\n")
+	fmt.Fprintf(w, "Version: 1.0.0\n")
+	fmt.Fprintf(w, "Hostname: %s\n", host)
+}
+
+func GetHeaders(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s with %s", r.URL.Path, r.Method)
+
+	fmt.Println("Query")
+	quries := r.URL.Query()
+	if val, ok := quries["key"]; ok {
+		fmt.Println(len(quries), val)
+	} else {
+		fmt.Fprintf(w, "No key in query: %s\n", "key")
+	}
+
+	for k, v := range r.URL.Query() {
+		fmt.Fprintf(w, "%s: %s\n", k, v)
+	}
+
+	for name, headers := range r.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func MultiHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/add" {
+		http.NotFound(w, r)
+		return
+	}
+	switch r.Method {
+	case "GET":
+		quries := r.URL.Query()
+		if val, ok := quries["a"]; ok {
+			fmt.Println(len(quries), val)
+			i, _ := strconv.Atoi(val[0])
+			fmt.Fprintf(w, "result: %d\n", i*2)
+		} else {
+			fmt.Fprintf(w, "No key in query: %s\n", "key")
+		}
+
+	case "POST":
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	mux.HandleFunc("/headers", GetHeaders)
+	mux.HandleFunc("/add", MultiHandler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Server listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, mux))
+}
